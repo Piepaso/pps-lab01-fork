b24565690b7198e16d059034fdec4afca462447e
Pietro Pasini - pietro.pasini4@studio.unibo.it <pietro.pasini4@studio.unibo.it>
2026-02-23 13:07:13 +0100

    smart door

---
+++ tdd-exercise/src/main/java/tdd/SmartDoorLockImpl.java
@@ -0,0 +1,70 @@
+package tdd;
+
+public class SmartDoorLockImpl implements SmartDoorLock {
+
+    private final int MAX_ATTEMPTS = 5;
+    private final int MAX_PIN = 9999;
+    private boolean locked;
+    private int pin;
+    private boolean isPinSet;
+    private int failedAttempts;
+
+    @Override
+    public void setPin(int pin) {
+        if (pin < 0 || pin > MAX_PIN) {
+            throw new IllegalArgumentException("Invalid Pin");
+        }
+        if (this.isLocked()) {
+            throw new IllegalStateException("Door is Locked");
+        } else  {
+            this.isPinSet = true;
+            this.pin = pin;
+        }
+    }
+
+    @Override
+    public void unlock(int pin) {
+        final boolean isPinWrong = ! (pin == this.pin);
+        if (isPinWrong) {
+            this.failedAttempts++;
+        } else  {
+            this.locked = false;
+        }
+    }
+
+    @Override
+    public void lock() {
+        if (!this.isPinSet) {
+            throw new IllegalStateException("Pin is not set");
+        }
+        this.locked = true;
+    }
+
+    @Override
+    public boolean isLocked() {
+        return this.locked;
+    }
+
+    @Override
+    public boolean isBlocked() {
+        return failedAttempts > getMaxAttempts();
+    }
+
+    @Override
+    public int getMaxAttempts() {
+        return MAX_ATTEMPTS;
+    }
+
+    @Override
+    public int getFailedAttempts() {
+        return failedAttempts;
+    }
+
+    @Override
+    public void reset() {
+        locked = false;
+        pin = 0;
+        isPinSet = false;
+        failedAttempts = 0;
+    }
+}
--- tdd-exercise/src/test/java/tdd/SmartDoorLockTest.java
+++ tdd-exercise/src/test/java/tdd/SmartDoorLockTest.java
@@ -1,12 +1,111 @@
 package tdd;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 public class SmartDoorLockTest {
+
+    private final int PIN = 1234;
+    private SmartDoorLock door;
+    private final int wrongPin = PIN + 1;
+
+    @BeforeEach
+    public void beforeEach(){
+        door = new SmartDoorLockImpl();
+    }
+
     @Test
-    public void todo() {
-        assertTrue(true);
+    void testDoorIsInitiallyUnlocked() {
+        assertFalse(door.isLocked());
+    }
+
+    @Test
+    void testDoorIsNoTInitiallyBlocked() {
+        assertFalse(door.isBlocked());
+    }
+
+    @Test
+    void testLockIfPinIsNotSet() {
+        assertThrows(IllegalStateException.class, () -> door.lock());
+    }
+
+    @Test
+    void testSetPin() {
+        door.setPin(PIN);
+    }
+
+    @Test
+    void testSetInvalidPin() {
+        assertThrows(IllegalArgumentException.class, () -> door.setPin(10000));
+        assertThrows(IllegalArgumentException.class, () -> door.setPin(-1));
+    }
+
+    @Test
+    void testLock() {
+        setPinAndLock();
+        assertTrue(door.isLocked());
+    }
+
+    @Test
+    void testCannotSetPinIfLocked() {
+        setPinAndLock();
+        assertThrows(IllegalStateException.class, () -> door.setPin(PIN));
+        assertTrue(door.isLocked());
+    }
+
+    @Test
+    void testUnlock()  {
+        setPinAndLock();
+        door.unlock(PIN);
+        assertFalse(door.isLocked());
+    }
+
+    @Test
+    void testWrongPinUnlock() {
+        setPinAndLock();
+        door.unlock(wrongPin);
+        assertTrue(door.isLocked());
+    }
+
+    @Test
+    void testGetFailedAttemptsNumber() {
+        setPinAndLock();
+        door.unlock(wrongPin);
+        door.unlock(PIN);
+        door.lock();
+        for (int i = 0; i < door.getMaxAttempts(); i++) {
+            door.unlock(wrongPin);
+        }
+
+    }
+
+    @Test
+    void testBlock() {
+        block();
+        assertTrue(door.isBlocked());
+    }
+
+    @Test
+    void testReset() {
+        block();
+        door.reset();
+        assertFalse(door.isBlocked());
+        assertFalse(door.isLocked());
+        assertThrows(IllegalStateException.class, () -> door.lock());
+        assertEquals(0, door.getFailedAttempts());
+    }
+
+    private void setPinAndLock() {
+        door.setPin(PIN);
+        door.lock();
+    }
+
+    private void block() {
+        setPinAndLock();
+        for (int i = 0; i <= door.getMaxAttempts(); i++) {
+            door.unlock(wrongPin);
+        }
     }
 }
